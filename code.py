b0060ba2ecf06f1882b30b10d119bed040945c21
hiralinda <lilirezerata@gmail.com>
2022-07-20 21:14:25 -0400

    add lizard and spock

--- code.py
+++ code.py
@@ -1,40 +1,30 @@
 import random
-options = ['rock', 'paper', 'scissors']
-
-def again():
-    again = input("do you want to try again? type yes or no: ")
-    again = again.lower()
-    if again == 'y' or 'yes':
-        play()
 
 
 def play():
-    user = input("Choose: rock, paper or scissors\n")
+    user = input("Choose: rock, paper, scissors, lizard or spock\n")
     user = user.lower()
-    computer = random.choice(['rock', 'paper', 'scissors'])
+    computer = random.choice(['rock', 'paper', 'scissors', 'lizard', 'spock'])
 
-    if user in options:
-        if user == computer:
-            print(f"Computer chose {computer} so, ")
-            print ('It\'s a tie')
-            again()
 
-        elif is_win(user, computer):
-            print(f"Computer chose {computer} so, ")
-            print ('You won')
-            again()
+    if user == computer:
+        print(f"Computer chose {computer} so, ")
+        print ('It\'s a tie')
 
-        else:
-            print(f"Computer chose {computer} so, ")
-            print ('You lost')
-            again()
+    elif is_win(user, computer):
+        print(f"Computer chose {computer} so, ")
+        print ('You won')
+
     else:
-        print("Please type rock, paper or scissors")
-        play()
+        print(f"Computer chose {computer} so, ")
+        print ('You lost')
 
 def is_win(player, opponent):
     if(player == 'rock' and opponent == 'scissors') or (player == 'scissors' and opponent == 'paper')\
-        or (player == 'paper' and opponent == 'rock'):
+        or (player == 'paper' and opponent == 'rock') or (player == 'rock' and opponent == 'lizard') \
+        or (player == 'scissors' and opponent == 'lizard') or (player == 'paper' and opponent == 'spock') \
+        or (player == 'spock' and opponent == 'rock') or (player == 'spock' and opponent == 'scissors') \
+        or (player == 'lizard' and opponent == 'spock') or (player == 'lizard' and opponent == 'paper'):
         return True
 
 play()
